ab8a56fd77851ea5db692c9742b7c4ed931869cb
Olya <63007250+olya2808@users.noreply.github.com>
2021-09-12 03:55:00 +0300

    Create help.h

---
+++ ЛР№5/help.h
@@ -0,0 +1,133 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+//создание класса матриц
+class Matrix
+{
+public:
+	vector<vector<double>> Mtrx;
+	int m, n;
+	//создание матрицы
+	void Create();
+	Matrix() : m(2), n(2)
+	{
+		Create();
+	}
+	Matrix(int i) : m(i), n(i)
+	{
+		Create();
+	}
+	Matrix(int i, int j) : n(i), m(j)
+	{
+		Create();
+	}
+	//очищение
+	~Matrix()
+	{
+		Mtrx.clear();
+	}
+	//перегрузка оператора сложения
+	void operator+(Matrix &B);
+	//перегрузка оператора вычитания
+	Matrix operator-(Matrix &B);
+	//перегрузка оператора умножения на число
+	Matrix operator*(double mul);
+	//перегрузка оператора умножения матриц
+	Matrix operator*(Matrix &B);
+	//перегрузка оператора ввода
+	void operator>>(ofstream &ft);
+	//перегрузка оператора вывода
+	Matrix operator<<(ifstream &ft);
+	//запись в бинарный файл
+	void BinW(ofstream &fb);
+	//считывание из бинарного файла
+	Matrix BinR(ifstream &fb);
+	//доступ к элементу матрицы
+	double &Fill(int i, int j);
+	//вывод матриц на экран
+	void Out(int signal);
+	//произведение Адамара
+	Matrix Adamar(Matrix& B);
+	//нахождение следа матрицы
+	double Trace();
+	//элементарные преобразования
+	Matrix ElementaryTr(int flag);
+	//нахождение определителя матрицы методом Гаусса
+	double Determinate();
+	//скалярное умножение векторов
+	double MultScal(Matrix& B);
+	//векторное умножение векторов
+	Matrix MultOut(Matrix& B);
+	//Евклидова норма
+	double EvNorm();
+	//максимальная норма
+	double MaxNorm();
+	//норма матрицы
+	double FrobNorm();
+	//нахождение угла между векторами
+	double Angle(Matrix& B);
+	//нахождение ранга матрицы
+	int Rank();
+	//нахождение миноров матрицы
+	Matrix Minor(int a, int b);
+	//нахождение обратной матрицы
+	Matrix InvertM();
+	//транспонирование
+	Matrix Transpose();
+	//добавление столбца в матрицу
+	Matrix Add(Matrix& t);
+	//нахождение нормы
+	double Norm();
+};
+//подкласс единичных матриц
+class Elementary : public Matrix
+{
+public:
+	Elementary(int i) : Matrix(i, i)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			Mtrx.at(j).at(j) = 1;
+		}
+	}
+};
+//подкласс диагональных матриц
+class Diagonal : public Elementary
+{
+public:
+	Diagonal(int i) : Elementary(i) {}
+};
+//подкласс верхнетреугольных матриц
+class UpperTriang : public Matrix
+{
+public:
+	UpperTriang(int i) : Matrix(i, i) {}
+};
+//подкласс нижнетреугольных матриц
+class DownTriang : public Matrix
+{
+public:
+	DownTriang(int i) : Matrix(i, i) {}
+};
+//подкласс симметричных матриц
+class Simmetric : public Matrix
+{
+public:
+	Simmetric(int i) : Matrix(i, i) {}
+};
+//класс PCA
+class PCA 
+{
+public:
+	//центрирование
+	void Centr(Matrix& B);
+	//шкалирование
+	void Scal(Matrix& B);
+	//алгоритм NIPALS
+	void NIPALS(Matrix& X);
+	vector<double> Leverage(Matrix& T);
+	//double TRV(int A);
+	//double ERV(int A);
+	//vector<double> Deviation(int A);
+};
